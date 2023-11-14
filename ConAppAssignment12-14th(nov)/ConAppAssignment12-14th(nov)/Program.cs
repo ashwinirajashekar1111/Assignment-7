18b17f41a398866581b05589f12c2eb0ae26f3a1
AshwiniRajashekar <ashwinirajashekar1111@gmail.com>
2023-11-14 15:31:28 +0530

    Assignment-7

---
+++ ConAppAssignment12-14th(nov)/ConAppAssignment12-14th(nov)/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.IO;
+
+namespace ConAppAssignment12_14th_nov_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose an operation: \n1. Create\n2. Read\n3. Append\n4. Delete");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Enter file name:");
+                    string createFileName = Console.ReadLine();
+                    Console.WriteLine("Enter content:");
+                    string createFileContent = Console.ReadLine();
+                    CreateFile(createFileName, createFileContent);
+                    break;
+
+                case 2:
+                    Console.WriteLine("Enter file name to read:");
+                    string readFile = Console.ReadLine();
+                    ReadFile(readFile);
+                    break;
+
+                case 3:
+                    Console.WriteLine("Enter file name to append:");
+                    string appendFileName = Console.ReadLine();
+                    Console.WriteLine("Enter content to append:");
+                    string appendContent = Console.ReadLine();
+                    AppendToFile(appendFileName, appendContent);
+                    break;
+
+                case 4:
+                    Console.WriteLine("Enter file name to delete:");
+                    string deleteFileName = Console.ReadLine();
+                    DeleteFile(deleteFileName);
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid choice");
+                    break;
+            }
+        }
+
+        static void CreateFile(string fileName, string content)
+        {
+            try
+            {
+                File.WriteAllText(fileName, content);
+                Console.WriteLine("File created successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error creating file: {ex.Message}");
+            }
+        }
+
+        static void ReadFile(string fileName)
+        {
+            try
+            {
+                string content = File.ReadAllText(fileName);
+                Console.WriteLine($"File content:\n{content}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error reading file: {ex.Message}");
+            }
+        }
+
+        static void AppendToFile(string fileName, string content)
+        {
+            try
+            {
+                File.AppendAllText(fileName, content);
+                Console.WriteLine("Content appended successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error appending to file: {ex.Message}");
+            }
+        }
+
+        static void DeleteFile(string fileName)
+        {
+            try
+            {
+                File.Delete(fileName);
+                Console.WriteLine("File deleted successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error deleting file: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ ConAppAssignment12-14th(nov)/ConAppAssignment12-14th(nov)/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ConAppAssignment12-14th(nov)")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConAppAssignment12-14th(nov)")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c64ea7d8-6341-4f42-b7af-7299de191757")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
